1f3a9b8c7f2c8e6b8b1664a2269223ffd918e416
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-03-09 09:25:13 -0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,4 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.indent.indent-blankline-nvim" },
-  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
 }
