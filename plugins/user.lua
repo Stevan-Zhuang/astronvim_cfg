68b64fce96176788a6bbeb65c09eef5d000c7cd8
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-03-22 16:19:57 -0700

    fix colorscheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,32 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    ---@type CatppuccinOptions
+    opts = {
+      transparent_background = false,
+      integrations = {
+        alpha = true,
+        aerial = true,
+        dap = true,
+        dap_ui = true,
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay0 },
+        }
+      end,
+    },
+  },
 }
